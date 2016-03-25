d293838759fae44fd8cfa96205ed47f9dbae169d
SeanPonce <sean@server.fake>
2016-03-25 08:00:06 -0500

    More webpack config
    Configured:
    - html-webpack-plugin
    - hot module replacement

--- app/index.js
+++ app/index.js
@@ -1,1 +1,5 @@
+var hello = document.createElement('h1');
+hello.innerHTML = "Hello Sean";
 
+var app = document.getElementById('app');
+app.appendChild(hello);
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,45 @@
+const webpack = require('webpack');
 const merge = require('webpack-merge');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const TARGET = process.env.npm_lifecycle_event;
-
-const build_dir = 'build';
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
 
 const common = {
-  entry: {
-    app: 'app'
-  },
+  entry: PATHS.app,
   output: {
-    path: build_dir,
+    path: PATHS.build,
     filename: 'bundle.js'
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.ejs',
+      title: 'My First React App',
+      appDivId: 'app'
+    })
+  ]
 };
 
 if (TARGET === 'start' || !TARGET) {
-  module.exports = merge(common, {});
+  module.exports = merge(common, {
+    devServer: {
+      contentBase: PATHS.build,
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+      stats: 'errors-only',
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
 }
 
 if (TARGET === 'build' || !TARGET) {
